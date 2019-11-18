afd3856fc465e6da065600c652f96c3967f4f646
Abhijith Nair <47491248+abhipn@users.noreply.github.com>
2019-11-19 05:00:10 +0530

    fixing cnn architecture
    Updated CNN architecture for better performance.

--- train_model.py
+++ train_model.py
@@ -10,35 +10,47 @@ import numpy as np
 import pandas as pd
 import helper as utl
     
-# preparing train and validation sets
-def prepare_data(data_dir, TRAIN_IMAGES, TEST_IMAGES):
+def load_filenames(train_n, dev_n, test_n):
+    """
+    Loading file names for train, cv and test sets.
+    """
+    dev_n  = train_n + dev_n + 1
+    test_n = dev_n + test_n + 1
     
-    train_images = list(map(lambda x: str(x)+'.jpg', 
-                             np.arange(TRAIN_IMAGES)))
+    train_imgs = list(map(lambda x: str(x) + '.jpg', np.arange(train_n)))
+    dev_imgs = list(map(lambda x: str(x) + '.jpg', np.arange(train_n, dev_n)))
+    test_imgs = list(map(lambda x: str(x) + '.jpg', np.arange(dev_n, test_n)))
+    return train_imgs, dev_imgs, test_imgs
 
-    # steering_angles
-    df = pd.read_csv('data.txt', delimiter=' |,', engine='python', 
-                     header=None, names=['img', 'angle', 'date', 'time'])
-    df.drop(['date', 'time'], axis=1, inplace=True)
-    df.set_index('img', inplace=True)
+def load_steering_angles(train_imgs, dev_imgs, test_imgs):
+    """
+    Loading angles for train, cv and test set.
+    """
+    columns = ['imgs', 'angles', 'date', 'time']
     
-    df = df.loc[train_images, 'angle']
+    data_df = pd.read_csv('data.txt', sep=' |,', engine='python', 
+                          header=None, names=columns).set_index('imgs')
     
-    train = df.iloc[:int(len(train_images)*.85)]
-    val = df.iloc[int(len(train_images)*.85):]
+    train_angles = data_df.loc[train_imgs]['angles'].to_numpy()
+    dev_angles = data_df.loc[dev_imgs]['angles'].to_numpy()
+    test_angles = data_df.loc[test_imgs]['angles'].to_numpy()
     
-    return (train.index, val.index), (np.deg2rad(train.values), np.deg2rad(val.values))
+    return train_angles, dev_angles, test_angles
 
-# Conv net architecture
+
+
 def custom_model(dropout_rate=0.3):
+    """
+    An Hybrid Model.
+    """
     base_model = vgg16.VGG16(include_top=False, weights='imagenet',
                              input_shape=utl.INPUT_SHAPE)
     
     for layer in base_model.layers[:7]:
         layer.trainable = False      
         
-    x = Conv2D(512, 7, activation='elu', strides=(2, 2))(base_model.layers[6].output)
-    x = Conv2D(256, 5, activation='elu', strides=(1, 1))(x)
+    x = Conv2D(256, 5, activation='elu', strides=(2, 2))(base_model.layers[6].output)
+    x = Conv2D(128, 3, activation='elu', strides=(1, 1))(x)
     x = Dropout(dropout_rate)(x)
     x = Flatten()(x)
     x = Dense(256, activation='elu')(x)
@@ -48,9 +60,15 @@ def custom_model(dropout_rate=0.3):
     model = Model(inputs=base_model.input, outputs=output_layer)
     return model
 
-# training the model
-def train_model(model, train_images, train_angles, val_images, val_angles, 
-				batch_size, nb_epochs, lr_rate=1e-04):
+def train_model(model, train_images, train_angles, 
+		val_images, 
+                val_angles, 
+		batch_size, 
+                nb_epochs, 
+                lr_rate=1e-04):
+    """
+    Training the model.
+    """
 
     # Callbacks
     log = CSVLogger('log.txt', append=True, separator=';')
@@ -60,35 +78,44 @@ def train_model(model, train_images, train_angles, val_images, val_angles,
                                  save_best_only=True,
                                  mode='auto')
     # Data Generators
-    train_angles_dict = dict(zip(train_images, train_angles))
-    train_gen = utl.DataGenerator(train_images, train_angles_dict, utl.data_dir, dim=(66, 200), 
-    							  batch_size=batch_size, shuffle=True, training=True)
+    train_angles_dict = dict(zip(train_imgs, train_angles))
+    dev_angles_dict = dict(zip(dev_imgs, dev_angles))
     
-    val_angles_dict = dict(zip(val_images, val_angles))
-    val_gen = utl.DataGenerator(val_images, val_angles_dict, utl.data_dir, dim=(66, 200), 
-    							  batch_size=batch_size, shuffle=False, testing=True)
+    train_gen = utl.DataGenerator(train_imgs, train_angles_dict, utl.data_dir, 
+                              dim=(utl.IMAGE_HEIGHT, utl.IMAGE_WIDTH), 
+                              batch_size=64, 
+                              shuffle=True, 
+                              training=True)
+    dev_gen = utl.DataGenerator(dev_imgs, dev_angles_dict, utl.data_dir, 
+                            dim=(utl.IMAGE_HEIGHT, utl.IMAGE_WIDTH), 
+                            batch_size=64, 
+                            shuffle=False,
+                            training=False)
 
-    model.compile(loss='mean_squared_error', optimizer=Adam(lr=lr_rate))
+    # Compiling and training the model.
+    model.compile(loss='mean_squared_error', optimizer=Adam(lr=lr_rate))    
     model.fit_generator(train_gen,
-                        steps_per_epoch=len(train_images)//batch_size,
+                        steps_per_epoch=utl.TRAIN_IMAGES//batch_size,
                         epochs=nb_epochs, 
-                        max_queue_size=40,
-                        validation_data=val_gen,
-                        validation_steps=len(val_images)//batch_size,
+                        max_queue_size=30,
+                        validation_data=dev_gen,
+                        validation_steps=utl.TEST_IMAGES//batch_size,
                         callbacks=[checkpoint, log],
                         verbose=1,
                         shuffle=True,
                         use_multiprocessing=True,
                         workers=10)
-    
+
 
 if __name__ == '__main__':
-    (train_images, val_images), (train_angles, val_angles) = prepare_data(utl.data_dir, utl.TRAIN_IMAGES, utl.TEST_IMAGES)
-
+    train_imgs, dev_imgs, test_imgs = load_filenames(utl.TRAIN_IMAGES, utl.DEV_IMAGES, utl.TEST_IMAGES)
+    train_angles, dev_angles, test_angles = load_steering_angles(train_imgs, dev_imgs, test_imgs)
+    
+    pickle.dump(test_imgs, open('test_imgs.pkl', 'wb'))
+    pickle.dump(test_angles, open('test_angles.pkl', 'wb'))
 
     # initializing the custom model
     model = custom_model()
-    #train_model(model, train_images, train_angles, val_images, val_angles, batch_size=32, nb_epochs=15)
-    print(model.summary())
+    train_model(model, train_imgs, train_angles, dev_imgs, dev_angles, batch_size=64, nb_epochs=15)
 
 
